e118d0bb089805817695a3380912b72047063d80
Michalis Anastasiou <mc.anastasiou@gmail.com>
2022-03-28 12:58:15 +0300

    Test pr

--- App.js
+++ App.js
@@ -32,15 +32,6 @@ const Section = ({children, title}): Node => {
     <View style={styles.sectionContainer}>
       <Text
         style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
           styles.sectionDescription,
           {
             color: isDarkMode ? Colors.light : Colors.dark,
